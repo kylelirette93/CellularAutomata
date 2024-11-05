373a35f6b4afdd8650d56c4a39ea10ec891f6c93
Kyle Lirette <162719550+kylelirette93@users.noreply.github.com>
2024-11-05 10:39:23 -0400

    Implemented cellular automaton

--- Assets/TestTilemap.cs
+++ Assets/TestTilemap.cs
@@ -8,7 +8,9 @@ public class TestTilemap : MonoBehaviour
 {
     public Tilemap myMap;
     public TileBase liveCell;
-    public int[,] multiDimensionalMap = new int[25, 25];
+    int neighbourCount;
+    public int[,] multiDimensionalMap = new int[15, 15];
+    public int[,] tempMap = new int[15, 15];
 
     private void Start()
     {
@@ -20,13 +22,14 @@ public class TestTilemap : MonoBehaviour
             }
         }
         DrawMap();
+        StartCoroutine(Automate());
     }
 
     void DrawMap()
     {
-        for (int y = 0; y < multiDimensionalMap.GetLength(1) - 1; y++)
+        for (int y = 0; y < multiDimensionalMap.GetLength(1); y++)
         {
-            for (int x = 0; x < multiDimensionalMap.GetLength(0) - 1; x++)
+            for (int x = 0; x < multiDimensionalMap.GetLength(0); x++)
             {
                 if (multiDimensionalMap[x, y] == 0)
                 {
@@ -35,27 +38,32 @@ public class TestTilemap : MonoBehaviour
                 else
                 {
                     myMap.SetTile(new Vector3Int(x, y, 0), liveCell);
-                    CountNeighbouringCells(x, y);
                 }
+                CountNeighbouringCells(x, y);
             }
         }
+        
     }
 
     int CountNeighbouringCells(int x, int y)
     {
-        int neighbourCount = 0;
-        // # # #
-        // _ * _    * = x 1 and y 1
-        // _ _ _
-         // x - 1 y -1  ,  x ,   x + 1
-         // x -1 y 
-        for (int newX = -1; newX < 2; newX++) 
+        neighbourCount = 0;
+       
+        for (int newX = -1; newX <= 1; newX++) 
         {
-            for (int newY = -1; newY < 2; newY++)
+            for (int newY = -1; newY <= 1; newY++)
             {
-                if (multiDimensionalMap[x + newX, y + newY] == 1)
+                int checkX = newX + x;
+                int checkY = newY + y;
+
+                // Keep within range of the map.
+                if (checkX >= 0 && checkX < multiDimensionalMap.GetLength(0) &&
+                checkY >= 0 && checkY < multiDimensionalMap.GetLength(1))
                 {
-                    neighbourCount++;
+                    if (multiDimensionalMap[checkX, checkY] == 1)
+                    {
+                        neighbourCount++;
+                    }
                 }
             }
         }
@@ -64,11 +72,56 @@ public class TestTilemap : MonoBehaviour
             neighbourCount--;
         }
         return neighbourCount;
+    }   
 
+    IEnumerator Automate()
+    {
+        while (true)
+        {
+            GenerateCells();
+            DrawMap();
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
-    
+    void GenerateCells()
+    {
+        for (int y = 0; y < multiDimensionalMap.GetLength(1); y++)
+        {
+            for (int x = 0; x < multiDimensionalMap.GetLength(0); x++)
+            {
+                neighbourCount = CountNeighbouringCells(x, y);
 
-    
-   
+                if (multiDimensionalMap[x, y] == 1)
+                {
+                    if (neighbourCount < 2 || neighbourCount > 3)
+                    {
+                        tempMap[x, y] = 0;
+                    }
+                    else if (neighbourCount == 3 || neighbourCount == 2)
+                    {
+                        tempMap[x, y] = 1;
+                    }
+                }
+                else if (multiDimensionalMap[x, y] == 0)
+                {
+                    if (neighbourCount == 3)
+                    {
+                        tempMap[x, y] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int y = 0; y < multiDimensionalMap.GetLength(1); y++)
+        {
+            for (int x = 0; x < multiDimensionalMap.GetLength(0); x++)
+            {
+                multiDimensionalMap[x, y] = tempMap[x, y];
+            }
+        }
+       
+        DrawMap();
+    }
 }
